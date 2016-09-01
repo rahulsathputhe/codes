8e7891ed67871ee76d774fcca4745ca4117ba007
rahulsathputhe <rahulsraljb@gmail.com>
2016-09-01 18:19:21 +0530

    largnum.cpp

--- largnum.cpp
+++ largnum.cpp
@@ -20,9 +20,16 @@ void printLargest(vector<string> a)
 	for (int i=0; i < a.size() ; i++ )
 		cout << a[i];
 }
-
 int main()
 {
 	vector<string> a;
+
+	a.push_back("54");
+	a.push_back("546");
+	a.push_back("548");
+	a.push_back("60");
+	printLargest(a);
+
+
 return 0;
 }
